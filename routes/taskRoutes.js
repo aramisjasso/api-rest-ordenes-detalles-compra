a800dd740a4d05f2728d66d152695546189e9f67
aramis <aramisjasso@gmail.com>
2025-03-04 08:59:03 -0700

    post-put-delete maybe ya funcionando

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,12 +1,22 @@
 // routes/taskRoutes.js
 import express from "express";
 import { getAllTasks, getTaskById, createTask, updateTask } from "../controllers/taskController.js";
+import { createOrder, updateOrder, deleteOrder } from "../controllers/post-put-deleteController.js";
 
 const router = express.Router();
 
-router.get("/", getAllTasks);
-router.get("/:id", getTaskById);
-router.post("/", createTask);
-router.put("/:id", updateTask);
+
+
+
+
+
+
+//RUTAS PARA ORDENES
+router.post("/", createOrder);
+router.put("/:id", updateOrder);
+router.delete("/:id", deleteOrder);
+
+
+
 
 export default router;
