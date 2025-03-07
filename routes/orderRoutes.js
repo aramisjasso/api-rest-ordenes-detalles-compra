b0e494a9299e2a39c749e2412de2d4665266805c
ferHaro03 <120071651+ferHaro03@users.noreply.github.com>
2025-03-06 20:05:27 -0700

    Update orderRoutes.js
    Había olvidado importar

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllOrders, getOrderById, getOrderProducts, getOrderDiscounts, getOrderShippingStatus, createOrder, updateOrder, deleteOrder } from "../controllers/orderController.js";
+import { getAllOrders, getOrderById, getOrderProducts, getOrderDiscounts, getOrderShippingStatus, createOrder, updateOrder, deleteOrder, updateOrderStatusByShipping } from "../controllers/orderController.js";
 
 const router = express.Router();
 
